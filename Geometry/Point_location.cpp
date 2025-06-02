82644834d35b91338df3ff6d2e4e0049677871c8
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-06-02 13:20:05 +0600

    Update Point_location.cpp

--- Geometry/Point_location.cpp
+++ Geometry/Point_location.cpp
@@ -1,4 +1,4 @@
-ll chk_sides(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3) {
+ll point_location(ll x1, ll y1, ll x2, ll y2, ll x3, ll y3) {
     ll m1 = (y2-y1) * (x3-x2);
     ll m2 = (x2-x1) * (y3-y2);
     if(m1 == m2) return 0; // TOUCH
