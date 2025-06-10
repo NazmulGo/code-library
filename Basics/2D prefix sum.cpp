99ee30d26a9363971e3f3cfe0ef50ac13161a3a4
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-06-11 00:59:49 +0600

    Update 2D prefix sum.cpp

--- Basics/2D prefix sum.cpp
+++ Basics/2D prefix sum.cpp
@@ -6,19 +6,20 @@ typedef long long ll;
 #define endl "\n" 
 #define all(v) v.begin(), v.end()
 #define fr(x, n) for (int i = x; i < n; ++i)
-const int N = 1001;
+const int N = 1000;
 
-int a[N][N], pref[N][N];
 void solve() {
     int n, m; cin >> n >> m;
+    vector<vll> arr(n+1, vll (m+1));
+    vector<vll> pref(n+1, vll (m+1));
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            cin >> a[i][j];
+            cin >> arr[i][j];
         }
     }
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + a[i][j];
+            pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + arr[i][j];
         }
     }
     int q; cin >> q;
