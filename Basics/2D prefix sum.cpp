78be8719aa32681dc9bea00b1c3d2a2d22561290
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-06-11 00:54:13 +0600

    Create 2D prefix sum.cpp

---
+++ Basics/2D prefix sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl "\n" 
+#define all(v) v.begin(), v.end()
+#define fr(x, n) for (int i = x; i < n; ++i)
+const int N = 2e5+123;
+
+int a[N][N], pref[N][N];
+void solve() {
+    int n, m; cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + a[i][j];
+        }
+    }
+    int q; cin >> q;
+    while (q--) {
+        int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        int ans = 0;
+        ans = pref[x2][y2] - pref[x1 - 1][y2] - pref[x2][y1 - 1] + pref[x1 - 1][y1 - 1];
+        cout << ans << endl;
+    }
+}
+
+// --- Try to formulate equations. ---
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    cout.tie(0);
+
+    ll tc = 1; // cin >> tc;
+    while(tc--) {
+        solve();
+    }
+}
