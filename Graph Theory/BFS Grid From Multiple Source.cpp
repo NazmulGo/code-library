0b2b8cb1be39539d39a45b0e856e5fe7f86148ff
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-06-13 10:46:12 +0600

    Update BFS Grid From Multiple Source.cpp

--- Graph Theory/BFS Grid From Multiple Source.cpp
+++ Graph Theory/BFS Grid From Multiple Source.cpp
@@ -1,3 +1,4 @@
+//https://cses.fi/problemset/task/1194/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -7,23 +8,20 @@ typedef long long ll;
 #define all(v) v.begin(), v.end()
 #define fr(x, n) for (int i = x; i < n; ++i)
 const int N = 1010;
-#define mem(a,b) memset(a, b, sizeof(a))
+
 #define yes cout << "YES" << endl
 #define no cout << "NO" << endl
 
 int dx[] = {0, 0, 1, -1, 1, -1, -1, 1};
 int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
 
-bool is_Possible = false;
-ll aa = 0, bb = 0;
-
 ll n, m;
 char arr[N][N];
 bool vis_mon[N][N], vis_me[N][N];
 pair<ll,ll>  par_me[N][N];
-vector<pll> monsters;
 pair<ll, ll> me;
 ll lvl_mon[N][N], lvl_me[N][N];
+vector<pll> monsters;
 
 void bfs_monsters() {
     queue<pll> q;
@@ -49,7 +47,6 @@ void bfs_monsters() {
     }
 }
 
-
 void bfs_me(ll x, ll y){
     par_me[x][y] = {-1, -1};
     vis_me[x][y] = true;
@@ -69,16 +66,12 @@ void bfs_me(ll x, ll y){
                 q.push({ii, jj});
                 par_me[ii][jj] = {i, j};
                 lvl_me[ii][jj] = 1 + lvl_me[i][j];
-           
-                if(is_Possible == false && (ii == 1 || ii == n || jj == 1 || jj == m) && ( lvl_me[ii][jj] < lvl_mon[ii][jj] || lvl_mon[ii][jj] == -1)) {
-                    is_Possible = true;
-                    aa = ii;
-                    bb = jj;
-                }
             }
         }
     }
 }
+
+
 
 void solve() {
     cin >> n >> m;
@@ -86,24 +79,35 @@ void solve() {
         for(int j=1; j<=m; j++) {
             cin >> arr[i][j];
             lvl_me[i][j] = -1, lvl_mon[i][j] = -1;
+            par_me[i][j] = {-1, -1};
             if(arr[i][j] == 'M') monsters.push_back({i, j});
-            if(arr[i][j] == 'A') me = {i, j};
+            if(arr[i][j] == 'A') me = {i, j}; 
         }
-    }
-
-    if(me.first == 1 || me.first == n || me.second == 1 || me.second == m) {
-        yes;
-        cout << 0 << endl;
-        return;
     }
 
     bfs_monsters();
     bfs_me(me.first, me.second);
-    if(!is_Possible) return void(no);
+
+    bool isPossible = false;
+    pair<ll, ll> p1;
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            if(i == 1 || j == 1 || i == n || j == m) {
+                if(lvl_me[i][j] != -1 && (lvl_me[i][j] < lvl_mon[i][j] || lvl_mon[i][j] == -1)){
+                    isPossible = true;
+                    p1 = {i, j};
+                    break;
+                }
+            }
+        }
+    }
+
+    if(!isPossible) return void(no);
     else yes;
 
-    ll x = aa, y = bb;
     string s;
+    ll x = p1.first, y = p1.second;
     while(par_me[x][y].first != -1) {
         auto [x1, y1] = par_me[x][y];
         if(x+1 == x1) s.push_back('U');
@@ -112,8 +116,10 @@ void solve() {
         else if(y-1 == y1) s.push_back('R');
         x = x1, y = y1;
     }
+
+    cout << s.size() << endl;
     reverse(all(s));
-    cout << s.size() << endl << s << endl;
+    cout << s << endl;
 }
 
 // --- Try to formulate equations. ---
