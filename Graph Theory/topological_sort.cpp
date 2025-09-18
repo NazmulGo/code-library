f8dc64a06db5ed3c5e99c83c88fd78cc3fadaeb8
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2025-09-18 11:20:44 +0600

    Create topological_sort.cpp

---
+++ Graph Theory/topological_sort.cpp
@@ -0,0 +1,37 @@
+ll n;
+bool vis[N];
+vector<ll> g[N], ans;
+stack<ll> st;
+
+void dfs(ll u) {
+    vis[u] = true;
+    for(auto v : g[u]) {
+        if(!vis[v]) {
+            dfs(v);
+        }
+    }
+    st.push(u);
+}
+
+
+void solve() {
+    cin >> n;
+    for(int i=0; i<n-1; i++) {
+        ll u, v; cin >> u >> v;
+        g[u].push_back(v);
+    }
+
+    for(int i=0; i<=5; i++) {
+        if(!vis[i]) {
+            dfs(i);
+        }
+    }
+
+    while(!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    for(int i=0; i<ans.size(); i++) cout << ans[i] << " ";
+    cout << endl; 
+
+}
