f7a7891fdcede8c1b93c8481b4642d3ea2fd66bb
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2026-01-15 23:45:56 +0600

    Update Topological_sort.cpp

--- Graph Theory/Topological_sort.cpp
+++ Graph Theory/Topological_sort.cpp
@@ -1,17 +1,25 @@
-/*
-7
-6 1
-5 1
-5 2
-4 2
-3 4
-6 3
-*/
+// Created on: 2026-01-15 23:21
+// By the hand of: Nazmul_Huda
 
-ll n;
-bool vis[N];
-vector<ll> g[N], ans;
-stack<ll> st;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define endl "\n" 
+#define all(v) v.begin(), v.end()
+const int N = 2e5+123;
+
+vector<ll> g[N]; 
+vector<bool> vis;
+vll par, lvl, ans;
+
+void g_clear(int n){
+    for(int i=0; i<=n; i++) g[i].clear();
+    par.assign(n+1, -1);
+    lvl.assign(n+1, 0);
+    vis.assign(n+1, false);
+}
 
 void dfs(ll u) {
     vis[u] = true;
@@ -20,28 +28,46 @@ void dfs(ll u) {
             dfs(v);
         }
     }
-    st.push(u);
+    ans.push_back(u);
 }
 
-
 void solve() {
-    cin >> n;
-    for(int i=1; i<n; i++) {
+
+    ll n, m; cin >> n >> m;
+    g_clear(n);
+    for(int i=1; i<=m; i++) {
         ll u, v; cin >> u >> v;
         g[u].push_back(v);
     }
 
-    for(int i=1; i<=n-1; i++) {
-        if(!vis[i]) {
-            dfs(i);
+    for(int i=1; i<=n; i++) if(!vis[i]) dfs(i);
+
+    reverse(all(ans));
+    vll mp(n+1, 0);
+    for(int i=0; i<ans.size(); i++) mp[ans[i]] = i;
+
+    bool ok = true;
+    for(int i=1; i<=n; i++) {
+        for(auto u : g[i]) {
+            if(mp[i] > mp[u]) {
+                ok = false; 
+            }
         }
     }
-
-    while(!st.empty()) {
-        ans.push_back(st.top());
-        st.pop();
+    
+    if(!ok) cout << "IMPOSSIBLE" << endl;
+    else {
+        for(int i=0; i<n; i++) cout << ans[i] << " ";
     }
-    for(int i=0; i<ans.size(); i++) cout << ans[i] << " ";
-    cout << endl; 
+}
 
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    cout.tie(0);
+
+    ll tc = 1; // cin >> tc;
+    while(tc--) {
+        solve();
+    }
 }
