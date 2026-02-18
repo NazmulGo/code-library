453d98ff738c1baa98c81dc26446af553c223906
Nazmul Huda <152804865+NazmulGo@users.noreply.github.com>
2026-02-19 02:30:21 +0600

    Update Monotonic_Stack.cpp

--- Data Structure/Monotonic_Stack.cpp
+++ Data Structure/Monotonic_Stack.cpp
@@ -1,34 +1,28 @@
 vll NextGreterEle(vll &v) {  
-    // stack design Decreasing  (Down to Up)
+    // stack design Increasing (Up to Down)
     ll n = v.size();
     vll ans(n);
     stack<ll> st;
     for(int i=n-1; i>=0; i--) {
-        while(!st.empty() && (st.top() <= v[i])) {
-            st.pop();
-        }
+        while(!st.empty() && (st.top() <= v[i])) st.pop();
         if(st.size() == 0) ans[i] = -1;
         else ans[i] = st.top();
         st.push(v[i]);
     } 
-
     return ans;
 }
 
 
 vll NextGreterEleIndex(vll &v) {  
-    // stack design Decreasing  (Down to Up)
+    // stack design Increasing (Up to Down)
     ll n = v.size();
     vll ans(n);
     stack<ll> st;
     for(int i=n-1; i>=0; i--) {
-        while(!st.empty() && (v[st.top()] <= v[i])) {
-            st.pop();
-        }
+        while(!st.empty() && (v[st.top()] <= v[i])) st.pop();
         if(st.size() == 0) ans[i] = -1;
         else ans[i] = st.top();
         st.push(i);
     } 
-
     return ans;
 }
